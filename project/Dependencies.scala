1ddaa2d90646df4fb0c41328c66caf7f7ae193bc
kirill5k <immotional@aol.com>
2020-12-28 18:41:59 +0000

    update fs2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    lazy val fs2        = "2.4.6"
+    lazy val fs2        = "2.5.0"
     lazy val pureConfig = "0.14.0"
     lazy val circe      = "0.13.0"
     lazy val sttp       = "2.2.9"
